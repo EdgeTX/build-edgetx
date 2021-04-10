5cf470c6341db59e1d58c1e18078cfeef77b330e
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-04-10 19:57:04 +0200

    Added work-around to run without LeakSanetizer

--- commit-tests/tests.sh
+++ commit-tests/tests.sh
@@ -2,6 +2,10 @@
 
 source /opt/qt$QT_BASE/bin/qt$QT_BASE-env.sh
 
+# LeakSanetizer needs to be disabled on docker,
+# or container run with '--privileged'
+export ASAN_OPTIONS=detect_leaks=0
+
 cd /src && \
     FLAVOR=COMPANION ./tools/commit-tests.sh && \
     FLAVOR=ARM9X     ./tools/commit-tests.sh && \

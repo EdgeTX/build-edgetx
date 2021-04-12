27341a487959f426a9ee5a74b86b12c29a584f1d
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-04-12 15:03:03 +0200

    Updated test script and Dockerfile
    - test script accounts for variables being set in opentx-dev image.
    - Dockerfile uses now the base container from github container registry

--- commit-tests/tests.sh
+++ commit-tests/tests.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-source /opt/qt$QT_BASE/bin/qt$QT_BASE-env.sh
-
-# LeakSanetizer needs to be disabled on docker,
-# or container run with '--privileged'
-export ASAN_OPTIONS=detect_leaks=0
-
 cd /src && \
     FLAVOR=COMPANION ./tools/commit-tests.sh && \
     FLAVOR=ARM9X     ./tools/commit-tests.sh && \
